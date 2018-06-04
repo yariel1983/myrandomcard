fec2a53609c90bc48a4e6d92e84aebb2606a8903
Yariel <cubanboy4life@gmail.com>
2018-06-04 01:29:14 +0000

    myrandomcard1

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+    This is your main stylesheet, you should use it only
+    to include other styles into your web app.
+*/
+//include jquery into the bundle and store its contents into the $ variable
+import $ from "jquery";
+//include bootstrap npm library into the bundle
+import 'bootstrap';
+//include your own styles
+import '../style/index.scss';
+<
+
+<img id="mycard1">
+<script>
+document.getElementById("MyCard").src = "segunda_imagen.jpg";
+</script>
+
